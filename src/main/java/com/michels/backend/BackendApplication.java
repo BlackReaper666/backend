899c2d118843deaca2c8d8558c72d10ac3b7d99c
Michel <michel_schevernels@hotmail.com>
2017-09-30 22:26:16 +0200

    new branch

--- src/main/java/com/michels/backend/BackendApplication.java
+++ src/main/java/com/michels/backend/BackendApplication.java
@@ -2,7 +2,7 @@ package com.michels.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class BackendApplication {
 
